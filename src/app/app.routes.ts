83ae8ea445c375febf82f33276eafdcd9765f84c
Miranda Soulcelo <marcelofmiranda23@gmail.com>
2025-02-25 21:25:40 -0300

    Aplicando a primeira rota user.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '',pathMatch: 'full', redirectTo: 'user'},
+  {path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule)}
+];
