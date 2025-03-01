c8ab4151de4267d62ac0f158393963486e336747
Miranda Soulcelo <marcelofmiranda23@gmail.com>
2025-03-01 15:59:52 -0300

    Renomeação da pasta 'crud-meet-and-music' para 'frontend' e outras mudanças

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-  {path: '',pathMatch: 'full', redirectTo: 'user'},
-  {path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule)}
-];
