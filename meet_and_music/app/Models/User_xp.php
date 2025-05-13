62af40c1c7b5b65cb8ed560c8491be91c08da8e4
MirandaSoulcelo <MirandaSoulcelo@users.noreply.github.com>
2025-05-13 12:21:01 -0300

    Finalmente Lógica de xp funcionando corretamente!!!!!

--- meet_and_music/app/Models/User_xp.php
+++ meet_and_music/app/Models/User_xp.php
@@ -13,30 +13,17 @@ class User_xp extends Model
 
     public function adicionarXP($xpGanho)
     {
-        $this->xp_atual = intval($this->xp_atual);
-        $xpGanho = intval($xpGanho);
+        $this->xp_atual = intval($this->xp_atual) + intval($xpGanho); 
     
-        $this->xp_atual += $xpGanho;
-    
-        // Enquanto houver XP suficiente para subir de nível
-        while ($this->xp_atual >= $this->xpNecessarioParaProximoNivel()) {
-            $xpNecessario = $this->xpNecessarioParaProximoNivel();
-            $this->xp_atual -= $xpNecessario;
-            $this->nivel_atual++;
+       
+        while ($this->xp_atual >= 100) {
+            $this->nivel_atual++; 
+            $this->xp_atual -= 100; 
         }
     
         $this->save();
     }
-    
 
 
-    private function xpNecessarioParaProximoNivel()
-    {
-        return 100 * $this->nivel_atual; // Cada nível precisa de 100 XP a mais
-    }
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+   
 }
