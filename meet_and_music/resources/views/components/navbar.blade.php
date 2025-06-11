852661c5c34df004aca4fa36306145aeb8d77448
matheusoms <mathmatiasjr1@gmail.com>
2025-06-11 03:25:09 -0300

    update

--- meet_and_music/resources/views/components/navbar.blade.php
+++ meet_and_music/resources/views/components/navbar.blade.php
@@ -15,25 +15,10 @@
                 <a href="{{ route('ranking.index') }}" class="nav-link">Ranking</a>
                 <a href="{{ route('friends.index') }}" class="nav-link">Comunidade</a>
                 <a href="{{ route('meetings.index') }}" class="nav-link">Chamadas</a>
+                <a href="{{ route('profile.show') }}" class="nav-link">Perfil</a>
+                <a href="{{ route('logout') }}" class="nav-link">Sair</a>
             </nav>
         @endauth
-
-        <div class="flex-shrink-0">
-            @auth
-                <div class="profile-container">
-                    <a href="{{ route('profile.show') }}" class="profile-link" aria-label="Ver perfil">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="profile-icon">
-                            <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
-                        </svg>
-                    </a>
-                    <div class="profile-dropdown">
-                        <form method="POST" action="{{ route('logout') }}" role="none">
-                            @csrf
-                            <button type="submit" class="dropdown-item w-full text-left">Sair</button>
-                        </form>
-                    </div>
-                </div>
-            @endauth
 
             @guest
                 <nav class="flex items-center space-x-2">
